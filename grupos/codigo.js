7b3b7b1b821d8ea4e1ff6920455b2f0f0b5f8f55
GArthTex66 <gabrielarthur@unifei.edu.br>
2022-10-20 22:30:43 -0300

    criando preenchimento dinamico e jsons

--- grupos/codigo.js
+++ grupos/codigo.js
@@ -1,12 +1,63 @@
-import dados from '../info.json' assert {type:'json'}
 
-// function carregaDados(){
-//   dados.forEach(element => {
+  
+  let Grupos = [
+    {
+      id: "A",
+      paises: [
+          { name: 'Brasil', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+          { name: 'Sérvia', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+          { name: 'Suiça', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+          { name: 'Camarões', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+      ]
+   },
+   {
+    id: "B",
+    paises: [
+        { name: 'Brasil', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+        { name: 'Sérvia', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+        { name: 'Suiça', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+        { name: 'Camarões', bandeira: 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg' },
+    ]
+ },
+];
+  
+  function dados() {
+  
+    const body = document.querySelector("body");
+    const div = document.createElement("div");
+    div.classList.add("containerTabela");
     
-//   });
-//   console.log("funcionou");
-// }
+    Grupos.forEach((element,index) => {
+      
+      const table = document.createElement("table");
+        table.id = "grupo"+(index+1);
+        table.classList.add("tabela");
 
-// function busca() {
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
 
-// }
+        const tr1 = document.createElement("tr");
+        tr1.innerHTML = "<th>Grupo "+(element.id)+"</th>";
+        thead.appendChild(tr1);
+        table.appendChild(thead);
+
+        element.paises.forEach(pais =>
+            {
+              const tr = document.createElement("tr");
+              const td = document.createElement("td");  
+              td.innerHTML = `<img src = "${pais.bandeira}" alt="Bandeira_${pais.name}"> ${pais.name}`
+              tr.appendChild(td);
+              tbody.appendChild(tr)
+            } 
+        )
+        table.appendChild(tbody);
+        div.appendChild(table);
+    });
+    body.appendChild(div);
+  }
+  
+function carregaDados() {
+  dados();
+}
+  
+  
